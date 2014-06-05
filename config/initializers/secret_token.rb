ffb3b1c85954dd084d8823e933f77ce5fb6dcd3f
Jose Sanchez <hailex@live.com>
2014-06-05 21:00:42 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoryTeller::Application.config.secret_key_base = '5ae4ec7832e0b6eba54ee3d3bca86fddeea8f154f6504224424c1eb7a6e55667c123c741804ef255a8f3fab4f966f79114d4bb14adc4cd2b7fb146e53f584e76'
