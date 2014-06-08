c4369477d6f1f89eda08636f7df4e65769335856
Jose Sanchez <hailex@live.com>
2014-06-08 19:18:33 +0000

    App layout, sidebar, top nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ StoryTeller::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
-  #devise_scope :user do
-  #get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  #end
+  devise_scope :user do
+  get 'sign_out', :to => 'devise/sessions#destroy'#, :as => :destroy_user_session
+  end
 
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
