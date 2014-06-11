d5ecd134a32aeae50c8479d5a0b279b38a6701fd
Jose Sanchez <hailex@live.com>
2014-06-11 12:24:33 +0000

    Created Story resource, revamped layout, navbar, sidebar, landing page

---
+++ db/migrate/20140609161102_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.references :user, index: true
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
