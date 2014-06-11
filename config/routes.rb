490558d34a4d4a0064f32270f12e000f689a352e
Jose Sanchez <jparkers0012@gmail.com>
2014-06-11 21:36:22 +0000

    Refactored user controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ StoryTeller::Application.routes.draw do
   devise_scope :user do
   get 'sign_out', :to => 'devise/sessions#destroy'#, :as => :destroy_user_session
   end
+  resources :users, only: [:index, :update, :show]
 
   get 'about' => 'welcome#about'
+  
   root to: 'welcome#index'
 end
